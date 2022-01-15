1abc04d1e04bc91ecbb93b40402c0b4b47f7aac5
moonkorea00 <moonkorea00@gmail.com>
2022-01-16 01:14:43 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/29th moon/LoginJeewon/LoginJeewon.js
+++ src/pages/29th moon/LoginJeewon/LoginJeewon.js
@@ -10,38 +10,58 @@ function LoginJeewon() {
     navigate('/main-Jeewon');
   };
 
-  const [IDInput, handleIdInput] = useState('');
-  const [PWInput, handlePwInput] = useState('');
+  const validId = () => {
+    if (!IDInput.includes('@')) {
+      alert('아이디를 다시 확인해주세요');
+    } else {
+    }
+  };
+  // Mission 2 - IDInput state에 '@' 포함 여부 확인 함수
 
+  const [IDInput, handleIdInput] = useState(''); //Mission 1
+  const [PWInput, handlePwInput] = useState(''); //Mission 1
+  // const [isValid, setIsValid] = useState(false);
+  // 질문: state 사용해서 isValid 구현 가능 여부
   return (
     <div className="main-container">
       <main className="login-container">
         <span className="heading">Westargram</span>
         <div className="login-wrap">
           <input
-            className="id login-input"
+            className="id-input"
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
             onChange={e => {
-              handleIdInput(e.target.value);
+              handleIdInput(e.target.value); //Mission 1
             }}
           />
           <input
-            className="password login-input"
+            className="password-input"
             type="password"
             placeholder="비밀번호"
-            onchange={e => {
-              handlePwInput(e.target.value);
+            onChange={e => {
+              handlePwInput(e.target.value); //Mission 1
             }}
           />
           <Link to="/main-Jeewon">
-            <button className="loginBtn button">로그인</button>
+            <button
+              className={
+                IDInput.length && PWInput.length > 5
+                  ? 'loginBtn-active'
+                  : 'loginBtn-inactive'
+              }
+              // Mission 2 - 조건 충족 여부에 따라 className 부여
+              onClick={validId}
+              // Mission 2 - '@' 포함 여부 onClick으로 구현
+            >
+              로그인
+            </button>
           </Link>
           <div className="reminder">
             <a href="google.com">비밀번호를 잊으셨나요?</a>
           </div>
           <button onClick={goToMain} className="test">
-            테스트: Applied onClick function to button
+            테스트: onClick으로 다른 페이지 이동
           </button>
           {/* <button onClick={function(){}}></button> */}
         </div>
@@ -49,5 +69,23 @@ function LoginJeewon() {
     </div>
   );
 }
+
+// const goToMain = () => {
+//   if(response.message === "valid user"){
+//     navigate('/main');
+//   } else {
+//     alert("가입된 회원이 아닙니다. 회원가입을 먼저 해주세요.")
+//     navigate('/signup');
+//   }
+// }
+
+//   return (
+//     <div>
+//       <button className="loginBtn" onClick={goToMain}>
+//         로그인
+//       </button>
+//     </div>
+//   );
+// }
 
 export default LoginJeewon;
