1126a35d36e5fd5715a9add96c3d00670c204d53
재형's <jaehyungs@jaehyeongsui-MacBookAir.local>
2022-01-15 13:02:56 +0900

    Add:이미지파일 및 main페이지 수정

--- src/pages/29th-park/LoginJaehyung/LoginJaehyung.js
+++ src/pages/29th-park/LoginJaehyung/LoginJaehyung.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import './LoginJaehyung.scss';
+import { useNavigate } from 'react-router-dom';
 
 function JaehyungLogin() {
-  return <div className="loginJaehyung">재형 로그인 페이지</div>;
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/login-jaehyung');
+  };
+  return (
+    <div>
+      <div className="loginJaehyung">
+        <div className="border">
+          <div className="title">Westagram</div>
+          <input
+            className="user"
+            type="text"
+            placeholder="전화번호,사용자 이름 또는 번호"
+          />
+          <input className="password" type="password" placeholder="비밀번호" />
+          <button className="btn" onClick={goToMain}>
+            로그인
+            {/* <Link to="/main"></Link> */}
+          </button>
+          <span className="lostpassword">
+            <a href="/">비밀번호를 잊으셨나요?</a>
+          </span>
+        </div>
+        <script src="js/login.js" />
+      </div>
+      ;
+    </div>
+  );
 }
 
 export default JaehyungLogin;
--- src/pages/29th-park/MainJaehyung/MainJaehyung.js
+++ src/pages/29th-park/MainJaehyung/MainJaehyung.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import './MainJaehyung.scss';
 
 function JaehyungMain() {
-  return <div className="mainJaehyung">재형 메인 페이지</div>;
-}
+  return (
+    <div className="mainJaehyung">
+      <class className="westagram" />
 
+      <nav className="navigation">
+        <div className="logo_bar">
+          <img
+            className="logo_picture"
+            src="Images/Park/MainPark/westagram.png"
+            alt="인스타그램 로고"
+          />
+          <div className="vertical_bar" />
+          <span className="logo_name">instagram</span>
+        </div>
+        <div className="search_space">
+          <input className="search_bar" type="text" placeholder="검색" />
+        </div>
+        <div className="right_menu">
+          <img alt="탐색" src="images/Park/MainPark/explore.png" />
+          <img alt="하트" src="Images/Park/MainPark/heart.png" />
+          <img alt="마이페이지" src="Images/Park/MainPark/profile.png" />
+        </div>
+      </nav>
+      <class />
+
+      <div>
+        <main className="main">
+          <div className="feeds">
+            <article />
+          </div>
+          <div className="main_right" />
+        </main>
+        <div className="reple">
+          <input
+            className="reple_place"
+            type="text"
+            placeholder="댓글달기..."
+          />
+          <button className="reple_btn">게시</button>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default JaehyungMain;
