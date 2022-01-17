c7ca3dcf57838b4bfa07ba93ddfd60c4044f2ecf
재형's <jaehyungs@jaehyeongsui-MacBookAir.local>
2022-01-17 16:00:40 +0900

    :Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/29th-park/LoginJaehyung/LoginJaehyung.js
+++ src/pages/29th-park/LoginJaehyung/LoginJaehyung.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginJaehyung.scss';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function JaehyungLogin() {
-  const navigate = useNavigate();
-  const goToMain = () => {
-    navigate('/login-jaehyung');
+  const [changeId, setChangeId] = useState('');
+  const [changePw, setChangePw] = useState('');
+  // // const navigate = useNavigate();
+  // // const goToMain = () => {
+  // //   navigate('/login-jaehyung');
+  // };
+  const handleIdInput = e => {
+    setChangeId(e.target.value);
+  };
+  const handlePwInput = e => {
+    setChangePw(e.target.value);
   };
   return (
     <div>
@@ -13,14 +22,20 @@ function JaehyungLogin() {
         <div className="border">
           <div className="title">Westagram</div>
           <input
+            onChange={handleIdInput}
             className="user"
             type="text"
             placeholder="전화번호,사용자 이름 또는 번호"
           />
-          <input className="password" type="password" placeholder="비밀번호" />
-          <button className="btn" onClick={goToMain}>
+          <input
+            onChange={handlePwInput}
+            className="password"
+            type="password"
+            placeholder="비밀번호"
+          />
+          <button className="btn">
             로그인
-            {/* <Link to="/main"></Link> */}
+            <Link to="/main" />
           </button>
           <span className="lostpassword">
             <a href="/">비밀번호를 잊으셨나요?</a>
