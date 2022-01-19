4b0b8ef53dd10f8b7ded788ac3aaebe678729751
재형's <jaehyungs@jaehyeongsui-MacBookAir.local>
2022-01-20 01:36:26 +0900

    Add:Mission 3 - 댓글 기능 구현,Mission 4 - 댓글 컴포넌트화, props로 데이터 전달 구현,공용nav적용

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Nav.scss';
+
+function Nav() {
+  return (
+    <nav className="nav-main">
+      <div className="section-left">
+        <img
+          src="images/Moon/MainMoon/instagramLogo.png"
+          alt="instagram"
+          className="instalogo"
+        />
+      </div>
+      <div className="section-center">
+        <input type="text" placeholder="검색" className="search-box" />
+      </div>
+      <div className="section-right">
+        <img src="images/Moon/MainMoon/dmLogo.png" alt="dm" className="icon" />
+        <img
+          src="/images/Moon/MainMoon/heartLogo.png"
+          alt="heart"
+          className="icon icon-heart"
+        />
+        <img
+          src="images/Moon/MainMoon/homeLogo.png"
+          alt="home"
+          className="icon icon-home"
+        />
+        <img
+          src="images/Moon/MainMoon/profilepic.png"
+          alt="profile"
+          className="icon-profile"
+        />
+      </div>
+    </nav>
+  );
+}
+export default Nav;
--- src/pages/29th-park/MainJaehyung/MainJaehyung.js
+++ src/pages/29th-park/MainJaehyung/MainJaehyung.js
@@ -1,63 +1,64 @@
 import React, { useState } from 'react';
 import './MainJaehyung.scss';
+import Nav from '../../../components/Nav/Nav';
 
 function JaehyungMain() {
   const [newReple, setNewReple] = useState([]);
   const [repleInput, setRepleInput] = useState('');
 
-  const clickBtn = () => {
-    let newArray = [...newReple];
+  const clickBtn = e => {
+    const newArray = [...newReple];
     newArray.push(repleInput);
     setNewReple(newArray);
   };
   const enter = e => {
     if (e.key === 'Enter') {
       let newArray = [...newReple];
-      newArray.push(setNewReple);
+      newArray.push(repleInput);
       setNewReple(newArray);
     }
   };
   return (
-    <div className="mainJaehyung">
-      <nav className="navigation">
-        <div className="logo_bar">
-          <img
-            className="logo_picture"
-            src="Images/Park/MainPark/westagram.png"
-            alt="인스타그램 로고"
-          />
-          <div className="vertical_bar" />
-          <span className="logo_name">instagram</span>
+    <>
+      <Nav />
+      <div className="mainJaehyung">
+        <div className="main">
+          <div className="feed">
+            <article className="article">
+              <div>wecod_bootcamp</div>
+              <div>WeCode-위코드</div>
+              <img src="images/Park/MainPark/drum.webp" alt="drum" />
+              <div className="comment-box" />
+              {newReple.map(data => {
+                return <Newreple data={data} />;
+              })}
+              <div className="repleBox">
+                <input
+                  onChange={e => {
+                    setRepleInput(e.target.value);
+                  }}
+                  type="text"
+                  placeholder="댓글 달기..."
+                  onKeyDown={enter}
+                />
+                <button type="button" onClick={clickBtn}>
+                  게시
+                </button>
+              </div>
+            </article>
+          </div>
+          <div className="aside" />
         </div>
-        <div className="search_space">
-          <input className="search_bar" type="text" placeholder="검색" />
-        </div>
-        <div className="right_menu">
-          <img alt="탐색" src="images/Park/MainPark/explore.png" />
-          <img alt="하트" src="Images/Park/MainPark/heart.png" />
-          <img alt="마이페이지" src="Images/Park/MainPark/profile.png" />
-        </div>
-      </nav>
-      <div className="main">
-        <div className="feed">
-          <article className="article">
-            <div>wecod_bootcamp</div>
-            <div>WeCode-위코드</div>
-            <img src="images/Park/MainPark/drum.webp" alt="drum" />
-            <div className="repleBox">
-              <input
-                onChange={e => {
-                  setRepleInput(e.target.value);
-                }}
-                type="text"
-                placeholder="댓글 달기..."
-              />
-              <button onClick={clickBtn}>게시</button>
-            </div>
-          </article>
-        </div>
-        <div className="aside" />
       </div>
+    </>
+  );
+}
+
+function Newreple(props) {
+  return (
+    <div className="reple-text">
+      <span className="user-id">_2.22_v</span>
+      <span className="user-reple">{props.data}</span>
     </div>
   );
 }
