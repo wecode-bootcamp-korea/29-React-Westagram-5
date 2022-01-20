180838bfb78b09fd8ee157da0c22c92256293922
재형's <jaehyungs@jaehyeongsui-MacBookAir.local>
2022-01-20 15:55:49 +0900

    Live Review후 LoginPage  Refactoring

--- src/pages/29th-park/LoginJaehyung/LoginJaehyung.js
+++ src/pages/29th-park/LoginJaehyung/LoginJaehyung.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import './LoginJaehyung.scss';
-import { useNavigate } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
 
 function JaehyungLogin() {
   const [changeId, setChangeId] = useState('');
   const [changePw, setChangePw] = useState('');
   const [isActive, setIsActive] = useState(false);
   const navigate = useNavigate();
-  // const goToMain = () => {
-  //   navigate('/main-jaehyung');
-  // };
+  const goToMain = () => {
+    navigate('/main-jaehyung');
+  };
   const goToSignUp = () => {
     fetch('http://10.58.3.35:80/users/login', {
       method: 'POST',
@@ -31,52 +30,40 @@ function JaehyungLogin() {
     setChangePw(e.target.value);
   };
 
-  const isLoginAbled = () => {
+  const buttonAcctive = () => {
     return changeId.includes('@') && changePw.length > 5
       ? setIsActive(true)
       : setIsActive(false);
   };
-  // const idAcctiveBtn = e => {
-  //   setIdVal(e.target.value.includes('@'));
-  // };
-  // const pwAcctiveBtn = e => {
-  //   setPwVal(e.target.value.lenght >= 5);
-  // };
   return (
-    <div>
-      <div className="loginJaehyung">
-        <div className="border">
-          <div className="title">Westagram</div>
-          <input
-            onChange={handleIdInput}
-            className="user"
-            type="text"
-            placeholder="전화번호,사용자 이름 또는 번호"
-            onKeyUp={isLoginAbled}
-          />
-          <input
-            onChange={handlePwInput}
-            className="password"
-            type="password"
-            placeholder="비밀번호"
-            onKeyUp={isLoginAbled}
-          />
-          <button
-            className="btn"
-            // onClick={goToMain}
-            onClick={goToSignUp}
-            disabled={isActive ? false : true}
-          >
-            로그인
-            {/* <Link to="/main-jaehyung" /> */}
-          </button>
-          <span className="lostpassword">
-            <a href="/">비밀번호를 잊으셨나요?</a>
-          </span>
-        </div>
-        <script src="js/login.js" />
-      </div>
-      ;
+    <div className="loginJaehyung">
+      <div className="jaehyungPageTitle">Westagram</div>
+      <input
+        onChange={handleIdInput}
+        className="userId"
+        type="text"
+        placeholder="전화번호,사용자 이름 또는 번호"
+        onKeyUp={buttonAcctive}
+      />
+      <input
+        onChange={handlePwInput}
+        className="userPassword"
+        type="password"
+        placeholder="비밀번호"
+        onKeyUp={buttonAcctive}
+      />
+      <button
+        className="loginBtn"
+        onClick={goToMain}
+        // onClick={goToSignUp}
+        disabled={isActive ? false : true}
+      >
+        로그인
+        {/* <Link to="/main-jaehyung" /> */}
+      </button>
+      <span className="lostpassword">
+        <a href="/">비밀번호를 잊으셨나요?</a>
+      </span>
     </div>
   );
 }
